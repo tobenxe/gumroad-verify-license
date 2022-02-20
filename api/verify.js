6c1c6d1ac15fea2f09bd1ba243b62f748864a80a
tobe <33559650+tobenxe@users.noreply.github.com>
2022-02-20 21:42:27 +0000

    slight refactor

--- api/verify.js
+++ api/verify.js
@@ -17,10 +17,10 @@ module.exports = async (req, res) =>{
                             });
         
         if(response.data.success){
-            //Check if refunded or chargedback
-            return (!response.data.purchase.refunded && !response.data.purchase.chargebacked) ?  
-            res.status(200).json({isValid: true}) : 
-            res.status(200).json({isValid: false})
+            //Check if refunded or chargedback            
+            if(!response.data.purchase.refunded && !response.data.purchase.chargebacked){
+             return  res.status(200).json({isValid: true})
+            }
         }
         return res.status(200).json({isValid: false});
     }catch(err){
